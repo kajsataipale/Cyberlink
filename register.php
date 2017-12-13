cfdbbea6c3501057e265f30d098fb08ee98ce596
“Kajsa <31646322+kajsataipale@users.noreply.github.com>
2017-12-13 09:42:11 +0100

    added the basic app structure and changed some pages and titles

---
+++ register.php
@@ -0,0 +1,8 @@
+<?php require __DIR__.'/views/header.php'; ?>
+
+<article>
+    <h1>Register</h1>
+    <p>New member? Here´s were you fill in your information</p>
+</article>
+
+<?php require __DIR__.'/views/footer.php'; ?>
