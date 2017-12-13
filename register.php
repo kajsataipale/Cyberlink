79d4a372d2f5bed4efe6b5910259eb65fcc35567
“Kajsa <31646322+kajsataipale@users.noreply.github.com>
2017-12-13 09:49:29 +0100

    added a register form

--- register.php
+++ register.php
@@ -5,4 +5,33 @@
     <p>New member? Here´s were you fill in your information</p>
 </article>
 
+<article>
+
+
+    <form action="app/auth/login.php" method="post">
+        <div class="form-group">
+            <label for="email">User name</label>
+            <input class="form-control" type="name" name="username" placeholder="francisdarjeeling" required>
+            <small class="form-text text-muted">Please provide a user name</small>
+        </div><!-- /form-group -->
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input class="form-control" type="email" name="email" placeholder="francis@darjeeling.com" required>
+            <small class="form-text text-muted">Please provide your email address.</small>
+        </div>
+        <div class="form-group">
+            <label for="email">Biography</label>
+            <textarea class="form-control" type="bio" name="bio" placeholder="I´m a student and I like baseboll.." required></textarea>
+            <small class="form-text text-muted">Please provide your biography</small>
+        </div>
+
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" type="password" name="password" required>
+            <small class="form-text text-muted">Please provide the your password (passphrase).</small>
+        </div><!-- /form-group -->
+
+        <button type="submit" class="btn btn-primary">Submitt</button>
+    </form>
+</article>
 <?php require __DIR__.'/views/footer.php'; ?>
